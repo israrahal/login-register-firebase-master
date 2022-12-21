92e17337fceeafd3d1bc6f86f75ffc2d9e6180c4
isra rahal <121164116+israrahal@users.noreply.github.com>
2022-12-21 22:18:59 +0100

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,168 @@
+
+import React, { useEffect, useState } from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Input, NativeBaseProvider, Button, Icon} from 'native-base';
+
+import { useNavigation } from '@react-navigation/native';
+import {PageLogo} from './../components/styles';
+import { auth } from '../firebase'
+
+
+function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        navigation.replace("Home")
+      }
+    })
+
+    return unsubscribe
+  }, [])
+
+  const handleSignUp = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(userCredentials => {
+        const user = userCredentials.user;
+        console.log('Registered with:', user.email);
+      })
+      .catch(error => alert(error.message))
+  }
+
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(userCredentials => {
+        const user = userCredentials.user;
+        console.log('Logged in with:', user.email);
+      })
+      .catch(error => alert(error.message))
+  }
+
+  return (
+    <View style={styles.container}>
+      <PageLogo resizeMode="cover" source={require('./../assets/images.png')} />
+      <View style={styles.text2}>
+        <Text>Don't have an account? </Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Signup")} ><Text style={styles.signupText}> Sign up</Text></TouchableOpacity>
+      </View>
+      
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={text => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={handleLogin}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    
+
+    
+     
+      
+    </View>
+  );
+}
+
+export default () => {
+  return (
+    <NativeBaseProvider>
+     
+        <Login />
+      
+    </NativeBaseProvider>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '00ffff',
+    
+  },
+  LoginText: {
+    marginTop:100,
+    fontSize:30,
+    fontWeight:'bold',
+  },
+  Middle:{
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  text2:{
+    flexDirection:'row',
+    justifyContent:'center',
+    paddingTop:5
+  },
+  signupText:{
+    fontWeight:'bold'
+  },
+  emailField:{
+    marginTop:30,
+    marginLeft:15
+  },
+  emailInput:{
+    marginTop:10,
+    marginRight:5
+  },
+  buttonStyle:{
+    marginTop:30,
+    marginLeft:15,
+    marginRight:15
+  },
+  buttonStyleX:{
+    marginTop:12,
+    marginLeft:15,
+    marginRight:15
+  },
+  buttonDesign:{
+    backgroundColor:'#026efd'
+  },
+  lineStyle:{
+    flexDirection:'row',
+    marginTop:30,
+    marginLeft:15,
+    marginRight:15,
+    alignItems:'center'
+  },
+  imageStyle:{
+    width:80,
+    height:80,
+    marginLeft:20,
+  },
+  boxStyle:{
+    flexDirection:'row',
+    marginTop:30,
+    marginLeft:15,
+    marginRight:15,
+    justifyContent:'space-around'
+  },
+});
